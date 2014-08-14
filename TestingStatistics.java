fdb04375ea44900ae0cbca64d5ff377ecaf90423
JoshuaSkootsky <joshua.skootsky@gmail.com>
2014-08-13 20:12:18 -0700

    a basic test

---
+++ TestingStatistics.java
@@ -0,0 +1,51 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class TestingStatistics.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class TestingStatistics
+{
+    /**
+     * Default constructor for test class TestingStatistics
+     */
+    public TestingStatistics()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void standardDevCalculationInts()
+    {
+        Statistics statisti1 = new Statistics();
+        double[] a = {51,66,93,90,84};
+        assertEquals(15.9, statisti1.stddev(a), 0.1);
+    }
+}
+
